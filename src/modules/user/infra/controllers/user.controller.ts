7a58cfc14cd03b688dadac98442128b8df653eb3
Rodrigo <rodsussumu@gmail.com>
2022-08-14 22:30:50 -0300

    add swagger and postman file

--- src/modules/user/infra/controllers/user.controller.ts
+++ src/modules/user/infra/controllers/user.controller.ts
@@ -10,6 +10,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UserService } from '../../domain/services/user.service';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { UpdateUserDto } from '../dtos/update-user.dto';
@@ -19,21 +20,32 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Listar todos os usuários' })
+  @ApiResponse({ status: 200, description: 'Busca feita com sucesso' })
   async index() {
     return await this.userService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Buscar usuário pelo id' })
+  @ApiResponse({ status: 200, description: 'Usuário encontrado' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
   async show(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.userService.findById(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Cadastrar novo usuário' })
+  @ApiResponse({ status: 201, description: 'Usuário atualizado' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
   async create(@Body() data: CreateUserDto) {
     return this.userService.create(data);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Atualizar usuário' })
+  @ApiResponse({ status: 200, description: 'Usuário atualizado' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
   async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() data: UpdateUserDto,
@@ -42,6 +54,9 @@ export class UserController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Remover usuário' })
+  @ApiResponse({ status: 204, description: 'Usuário removido' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
   @HttpCode(HttpStatus.NO_CONTENT)
   async delete(@Param('id', new ParseUUIDPipe()) id: string) {
     await this.userService.delete(id);
--- src/modules/user/infra/dtos/create-user.dto.ts
+++ src/modules/user/infra/dtos/create-user.dto.ts
@@ -1,30 +1,38 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   nome: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   cep: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   logradouro: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   numero: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   complemento: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   cidade: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   estado: string;
